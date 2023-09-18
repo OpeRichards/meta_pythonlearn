b45569c02fa97bc49ba48a7d908b60d0ad786408
Opeyemi Richard <opeyemirichard@gmail.com>
2023-09-18 08:10:06 -0700

    Task 2 modified to print Over 45 | Under 45

---
+++ intro_python/3-num_list.py
@@ -0,0 +1,8 @@
+num_list = [33,42,5,66,77,22,16,79,36,62,78,43,88,39,53,67,89,11]
+
+#Change the print statement to “Over 45” and add an else condition with a print statement of “Under 45”
+for item in num_list:
+    if item > 45:
+        print(item, '-', 'Over 45')
+    else:
+        print(item, '-', 'Under 45')
